5edcc53c65bb7a264916e8fbc5cdf70085303f3c
SamJs0512 <2400591J@student.tp.edu.sg>
2026-02-12 00:08:35 +0800

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import joblib
+import sklearn
+
+st.write("Sklearn version on Streamlit:", sklearn.__version__)
 
 # ==============================
 # Load Model
